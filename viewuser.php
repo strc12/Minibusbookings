771b1050c608bf6c8f92510821a2b50d20d86d37
Evan135790 <gao.z@oundleschool.org.uk>
2026-06-22 12:56:03 +0100

    viewuser3

--- viewuser.php
+++ viewuser.php
@@ -1,16 +1,16 @@
-<?php
-    //print_r($_POST);
-    $tempuser='john.doe@example.com';
-    include_once("connection.php");
-   
-    $stmt1= $conn->prepare("SELECT * from tblstaff where email=:email");
-
-    
-    $stmt1->bindParam(":email",$tempuser);
-    $stmt1->execute();
-     while($row=$stmt1->fetch(PDO::FETCH_ASSOC)){
-        print_r($row);
-        #make a pretty form to show date
-        
-    }
+<?php 
+include_once('connection.php');
+//print_r($_POST);
+try {
+    $stmt = $conn->prepare("SELECT * FROM tblstaff WHERE StaffID = :StaffID");
+     $stmt->bindParam(":StaffID",$_POST["StaffID"]);
+    while($row=$stmt->fetch(PDO::FETCH_ASSOC))
+        {
+            //print_r($row);
+            echo($row["StaffID"]);
+            echo("<br>");
+        }
+} catch(PDOException $e) {
+    echo "Error: " . $e->getMessage();
+}
 ?>
