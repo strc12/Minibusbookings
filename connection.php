6b2f770d904107550f4ecfcfe27b04d6ac67c144
Shcno <mcgroarty.l@oundleschool.org.uk>
2026-06-22 11:16:34 +0100

    addUser1

--- connection.php
+++ connection.php
@@ -2,7 +2,7 @@
     // This page connects the user to the database.
     $servername="localhost";
     $username="root";
-    $password="root";
+    $password="password";
     $dbname = "minibus";
     try{
         $conn=new PDO("mysql:host=$servername;dbname=$dbname",$username,$password);
