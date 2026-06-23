4bff8d7968d6833ef328accc42b672d3acfb1c90
Shcno <mcgroarty.l@oundleschool.org.uk>
2026-06-23 09:33:58 +0100

    adduserpage

---
+++ adduserprocess.php
@@ -0,0 +1,41 @@
+<?php
+    header("Location: useradmin.php");
+    //print_r($_POST);
+    include_once("connection.php");
+    
+    //$username="bob";
+    /* if($_POST["Role"]=="Staff"){
+        $role=0;
+    }elseif($_POST["Role"]=="Driver"){
+        $role=1;
+    }elseif($_POST["Role"]=="Manager"){
+        $role=2;
+    } */
+   
+/*     if($_POST["Licencetodrive"]=="Coach"){
+        $Licencetodrive=0;
+    }elseif($_POST["Licencetodrive"]=="17 minibus"){
+        $Licencetodrive=1;
+    }elseif($_POST["Licencetodrive"]=="9 seater"){
+        $Licencetodrive=2;
+    }elseif($_POST["Licencetodrive"]=="Car"){
+        $Licencetodrive=3;
+    }else{
+        $Licensetodrive=4;
+    } */
+
+    $stmt1= $conn->prepare("INSERT INTO TblStaff
+    (StaffID, FirstName, Surname, Role, Password, Email, Phone, Initials, Licencetodrive)
+    VALUES
+    (NULL,:FirstName, :Surname, :Role, :Password, :Email, :Phone, :Initials, :Licencetodrive)
+    ");
+    $stmt1->bindParam(":FirstName",$_POST["FirstName"]);
+    $stmt1->bindParam(":Surname",$_POST["Surname"]);
+    $stmt1->bindParam(":Role",$_POST["Role"]);
+    $stmt1->bindParam(":Password",$_POST["Password"]);
+    $stmt1->bindParam(":Email",$_POST["Email"]);
+    $stmt1->bindParam(":Phone",$_POST["Phone"]);
+    $stmt1->bindParam(":Initials",$_POST["Initials"]);
+    $stmt1->bindParam(":Licencetodrive",$_POST["Licencetodrive"]);
+    $stmt1->execute();
+?>
