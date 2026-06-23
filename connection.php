6315c390fb4c15a404f990d6b51b042b7ac8b15f
alexchan0902 <chan.t1@oundleschool.org.uk>
2026-06-23 10:36:41 +0100

    password change

--- connection.php
+++ connection.php
@@ -2,7 +2,7 @@
     // This page connects the user to the database.
     $servername="localhost";
     $username="root";
-    $password="password";
+    $password="root";
     $dbname = "minibus";
     try{
         $conn=new PDO("mysql:host=$servername;dbname=$dbname",$username,$password);
