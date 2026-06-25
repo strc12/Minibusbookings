04d088dccde5f4b968533d8b1e85c60f9ade5bcc
alexchan0902 <chan.t1@oundleschool.org.uk>
2026-06-25 10:06:50 +0100

    change password customized

--- changepassword.php
+++ changepassword.php
@@ -1,83 +1,112 @@
 <!DOCTYPE html>
 <html lang="en">
 <?php
-    session_start();
-    if ($_SESSION["correctpassword"] != true){
-        header("Location: confirmpassword.php");
-    }
+session_start();
+
+if (!isset($_SESSION["correctpassword"]) || $_SESSION["correctpassword"] != true) {
+    header("Location: confirmpassword.php");
+    exit();
+}
 ?>
 
 <head>
-    <title>Password change</title>
-    <!-- Latest compiled and minified CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet">
+    <title>Change Password</title>
 
-    <!-- Latest compiled JavaScript -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js"></script>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/site.css" rel="stylesheet">
 </head>
 
 <body>
-    <div class="container mt-5">
-        <div class="row">
-            <div class="offset-md-3 col-md-6">
-                <span id="check_password_match"></span>
-                <form action="processchangepassword.php" method="POST">
-                    <div class="mb-3">
-                        <label for="password" class="form-label">Password</label>
-                        <input name="password" type="text" class="form-control" id="password">
-                    </div>
-                    <div class="mb-3">
-                        <label for="confirm_password" class="form-label">Confirm Password</label>
-                        <input name="confirm_password" type="text" class="form-control" id="confirm_password">
-                    </div>
-                    <button type="submit" id="submitBtn" class="btn btn-primary" disabled>
-                        Submit
-                    </button>
-                </form>
+
+<?php include 'includes/navbar.php'; ?>
+
+<div class="container mt-5">
+    <div class="row justify-content-center">
+        <div class="col-md-6">
+
+            <div class="card shadow-sm">
+                <div class="card-body p-4">
+
+                    <h3 class="text-center mb-4">Change Password</h3>
+
+                    <span id="check_password_match" class="d-block mb-3"></span>
+
+                    <form action="processchangepassword.php" method="POST">
+
+                        <div class="mb-3">
+                            <label for="password" class="form-label">
+                                New Password
+                            </label>
+                            <input
+                                name="password"
+                                type="password"
+                                class="form-control"
+                                id="password"
+                                required
+                            >
+                        </div>
+
+                        <div class="mb-4">
+                            <label for="confirm_password" class="form-label">
+                                Confirm Password
+                            </label>
+                            <input
+                                name="confirm_password"
+                                type="password"
+                                class="form-control"
+                                id="confirm_password"
+                                required
+                            >
+                        </div>
+
+                        <button
+                            type="submit"
+                            id="submitBtn"
+                            class="btn btn-primary w-100"
+                            disabled>
+                            Change Password
+                        </button>
+
+                    </form>
+
+                </div>
             </div>
+
         </div>
     </div>
+</div>
 
-    <!-- Latest compiled JavaScript -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.0/jquery.min.js"></script>
-    <script>
-        $(document).on('keyup', '#confirm_password', function () {
-            let password = $("#password").val();
-            let confirm_password = $("#confirm_password").val();
-            if (password != confirm_password) {
-                $("#check_password_match").html("Password does not match.").css("color", "red");
-            } else {
-                $("#check_password_match").html("")
-            }
-        });
-        $(document).on('keyup', '#password, #confirm_password', function () {
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.0/jquery.min.js"></script>
 
-            let password = $("#password").val();
-            let confirm_password = $("#confirm_password").val();
+<script>
+$(document).on('keyup', '#password, #confirm_password', function () {
 
-            if (password === "" || confirm_password === "") {
-                $("#check_password_match").html("");
-                $("#submitBtn").prop("disabled", true);
-            }
-            else if (password != confirm_password) {
-                $("#check_password_match")
-                    .html("Passwords do not match.")
-                    .css("color", "red");
-                $("#submitBtn").prop("disabled", true);
-            }
-            else {
-                $("#check_password_match")
-                    .html("Passwords match.")
-                    .css("color", "green");
-                $("#submitBtn").prop("disabled", false);
-            }
-        });
-    </script>
+    let password = $("#password").val();
+    let confirm_password = $("#confirm_password").val();
+
+    if (password === "" || confirm_password === "") {
+        $("#check_password_match").html("");
+        $("#submitBtn").prop("disabled", true);
+    }
+    else if (password !== confirm_password) {
+        $("#check_password_match")
+            .html("Passwords do not match.")
+            .css("color", "red");
+
+        $("#submitBtn").prop("disabled", true);
+    }
+    else {
+        $("#check_password_match")
+            .html("Passwords match.")
+            .css("color", "green");
+
+        $("#submitBtn").prop("disabled", false);
+    }
+});
+</script>
+
 </body>
 </html>
--- confirmpassword.php
+++ confirmpassword.php
@@ -1,24 +1,66 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
-    <title>Confirm password</title>
-    <!-- Latest compiled and minified CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet">
+    <title>Confirm Password</title>
 
-    <!-- Latest compiled JavaScript -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js"></script>
 
-    <form action="checkpassword.php" method="POST">
-        Confirm password: <input type="text" name="password"><br>
-        <?php
-        session_start();
-        if ($_SESSION["error"] == "Invalid password"){
-            echo "<p style='color:red'>" . $_SESSION["error"] . "</p>";
-            unset($_SESSION["error"]);
-        }
-        ?>
-        <input type="submit" value="submit">
-    </form>
-
+    <link href="css/site.css" rel="stylesheet">
 </head>
+
+<body>
+
+<?php
+session_start();
+include 'includes/navbar.php';
+?>
+
+<div class="container mt-5">
+    <div class="row justify-content-center">
+        <div class="col-md-6">
+
+            <div class="card shadow-sm">
+                <div class="card-body p-4">
+
+                    <h3 class="mb-4">Confirm Password</h3>
+
+                    <form action="checkpassword.php" method="POST">
+
+                        <div class="mb-3">
+                            <label for="password" class="form-label">
+                                Password
+                            </label>
+                            <input
+                                type="password"
+                                class="form-control"
+                                id="password"
+                                name="password"
+                                required
+                            >
+                        </div>
+
+                        <?php
+                        if (isset($_SESSION["error"])) {
+                            echo "<div class='alert alert-danger'>";
+                            echo $_SESSION["error"];
+                            echo "</div>";
+                            unset($_SESSION["error"]);
+                        }
+                        ?>
+
+                        <button type="submit" class="btn btn-primary">
+                            Submit
+                        </button>
+
+                    </form>
+
+                </div>
+            </div>
+
+        </div>
+    </div>
+</div>
+
+</body>
 </html>
